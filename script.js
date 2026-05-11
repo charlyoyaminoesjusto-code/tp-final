1a0e2ea43045b621f7f983e3b7607c54a7fe2983
charlyoyaminoesjusto-code <charlyo.yami.noesjusto@gmail.com>
2026-05-11 08:43:42 -0300

    actualizacion de tp final
    1era modificacion

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 function filtrar(cat) {
+
     const cards = document.querySelectorAll(".card");
 
     cards.forEach(card => {
+
         if (cat === "all" || card.dataset.categoria === cat) {
+
             card.style.display = "block";
+
         } else {
+
             card.style.display = "none";
         }
     });
+
+    /* 🔥 BOTON ACTIVO */
+
+    const botones = document.querySelectorAll(".filtros button");
+
+    botones.forEach(boton => {
+        boton.classList.remove("activo");
+    });
+
+    event.target.classList.add("activo");
 }
